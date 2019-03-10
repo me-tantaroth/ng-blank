eb679ae0aaf205d2ce7c65e0ae73815ba486bee8
Eduard Ramirez <me@tantaroth.com>
2019-03-09 21:40:47 -0500

    Remove declarations of NGModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material.module';
+import { AuthModule } from '../auth/auth.module';
+
+import { UkModalComponent } from './components/uk-modal/uk-modal.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [UkModalComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MaterialModule,
+    AuthModule
+  ],
+  exports: [UkModalComponent]
 })
 export class SharedModule { }
