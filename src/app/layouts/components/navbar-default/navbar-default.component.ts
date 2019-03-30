b5c2cb0bdf640ea98245e3d599732fd9afb0aba6
Eduard Ramirez <tantaroth@gmail.com>
2019-03-30 16:13:12 -0500

    Add menu to frontend navbar

--- src/app/layouts/components/navbar-default/navbar-default.component.ts
+++ src/app/layouts/components/navbar-default/navbar-default.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { AuthService } from '../../../core/auth/services/auth.service';
+import { MenuService } from '../../../core/menus/services/menu.service';
+
+import { Menu } from '../../../core/menus/models/menu';
 
 @Component({
   selector: 'app-navbar-default',
@@ -10,15 +14,45 @@ import { AuthService } from '../../../core/auth/services/auth.service';
 })
 export class NavbarDefaultComponent implements OnInit {
   authenticated: Observable<boolean>;
+  menu: Observable<Menu[]> = of([]);
 
-  constructor(private auth: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private menuService: MenuService
+  ) {}
 
   ngOnInit() {
-    this.authenticated = this.auth.authenticated;
+    this.authenticated = this.authService.authenticated;
+    this.menu = this.menuService.filter({
+      deleted: false
+    });
+  }
+
+  submenu(menu: Menu[]) {
+    let html = '';
+    if (menu && menu.length && menu.length > 0) {
+      for (let item of menu) {
+        html += `<li>`;
+          html += `<a href="#">${item.text}</a>`;
+          if (item.menu && item.menu.length && item.menu.length > 0) {
+            html += '<div class="uk-navbar-dropdown">';
+              html += '<ul class="uk-nav uk-navbar-dropdown-nav">';
+                html += this.submenu(item.menu);
+              html += '</ul>';
+            html += '</div>';
+          }
+        html += '</li>';
+      }
+    }
+
+    console.log(html);
+    return html;
   }
 
   signOut() {
-    this.auth.signOut().subscribe().unsubscribe();
+    this.authService
+      .signOut()
+      .subscribe()
+      .unsubscribe();
   }
-
 }
